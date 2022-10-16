ce770b1b600e8bf1011b303e72340e9824d6a104
Henrik Vollesen <henrik@vollesen.dk>
2022-10-16 17:19:19 +0200

    Controllers added. HttpsRedirection and MapControllers added in Program.cs. LaunchUrl added in launchSettings

---
+++ Data/CarRentalApiContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using CarRentalApi.Models;
+
+namespace CarRentalApi.Data
+{
+    public class CarRentalApiContext : DbContext
+    {
+        public CarRentalApiContext (DbContextOptions<CarRentalApiContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<CarRentalApi.Models.Car> Car { get; set; } = default!;
+
+        public DbSet<CarRentalApi.Models.Color> Color { get; set; }
+
+        public DbSet<CarRentalApi.Models.FuelType> FuelType { get; set; }
+
+        public DbSet<CarRentalApi.Models.Location> Location { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using CarRentalApi.Data;
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<CarRentalApiContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("CarRentalApiContext") ?? throw new InvalidOperationException("Connection string 'CarRentalApiContext' not found.")));
+
+builder.Services.AddControllers();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseHttpsRedirection();
+
+app.MapControllers();
+
+//app.MapGet("/", () => "Hello World!");
 
 app.Run();
