1d1ab8309c00fbe03922a9c8e7bc4c0bc7abacc5
Henrik Vollesen <henrik@vollesen.dk>
2022-10-17 05:53:07 +0200

    Started using Dapper

--- Models/CarsController.cs
+++ Models/CarsController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Dapper;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 
 namespace CarRentalApi.Models
@@ -8,11 +10,19 @@ namespace CarRentalApi.Models
     [ApiController]
     public class CarsController : ControllerBase
     {
-        //string connString = ConfigurationManager.ConnectionStrings["DapperConnStr"].ConnectionString;
+        string connString = "Data Source=HENRIK-DESKTOP;Integrated Security=True;Initial Catalog=CarRental-V1";
+        string sql1 = "SELECT * FROM Car";
+
+
         // GET: api/Cars
         [HttpGet]
         public async Task<ActionResult<IEnumerable<Car>>> GetCar()
         {
+            using (SqlConnection connection = new SqlConnection(connString))
+            {
+                var books = connection.Query<Car>(sql1).ToList();
+            }
+
             //return await _context.Car.ToListAsync();
             return NoContent();
         }
